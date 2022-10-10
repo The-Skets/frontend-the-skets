f5a7bf6f52e0ab54caa916a6b557b6d10bb0747f
Carter <Carterannandale@gmail.com>
2022-10-10 14:44:37 +0100

    Work on users

--- components/admin/users/AllUsers.jsx
+++ components/admin/users/AllUsers.jsx
@@ -11,11 +11,68 @@ export default function AllUsers() {
 
     return (
         <>
-            {data.map((user) => {
-                <div key={user.username}>
-                    {/* TODO: this. */}
+            <div className="flex flex-col overflow-auto overflow-y-hidden">
+                <div className="-my-2 sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-auto overflow-y-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead className="bg-gray-50">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        ID
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Username
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Email
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Account Type
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Date Joined
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Number of comments
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                {data.map((user) => (
+                                    <tr key={user.username}>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-600 hover:text-indigo-900"><Link href={"/admin/users/"+user.id}>{user.id}</Link></td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.username}</td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.email}</td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.account_type}</td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.date_joined}</td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.number_of_comments}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-            })}
+            </div>
         </>
     )
 }
